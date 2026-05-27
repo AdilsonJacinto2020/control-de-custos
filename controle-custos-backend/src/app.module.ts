11fad5201920ad194d91f7dfa4f078e5bc5c56f3
AdilsonJacinto2020 <adijacinto.aj@gmail.com>
2026-05-27 01:30:44 +0100

    up and runnig back and frontend

--- controle-custos-backend/src/app.module.ts
+++ controle-custos-backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DespesasController } from './despesas/despesas.controller';
+import { DespesasService } from './despesas/despesas.service';
+import { DespesasModule } from './despesas/despesas.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DespesasModule],
+  controllers: [AppController, DespesasController],
+  providers: [AppService, DespesasService],
 })
 export class AppModule {}
---
+++ controle-custos-backend/src/despesas/create-despesa.dto.ts
@@ -0,0 +1,7 @@
+export class CreateDespesaDto {
+	descricao: string;
+	valor: number;
+	data: string;
+	categoria: string;
+
+}
